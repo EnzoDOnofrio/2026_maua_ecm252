beaae96240f67fc6f1bc56ece2c1cb39106a8a09
enzo_donofrio <enzo.oliveira2004@outlook.com>
2026-03-07 10:40:06 -0300

    learn(ap003-react): encerra a apostila

--- ap003_react_jsx_componentes/src/App.jsx
+++ ap003_react_jsx_componentes/src/App.jsx
@@ -1,21 +1,40 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import './style.css'
 
 const App = () => {
-return(
-<div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding:
+    const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8,
+backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%',
+borderRadius: 8}
+
+const textoDoRotulo = "Nome:"
+
+const obterTextoDoBotao = () => 'Enviar'
+
+const aoClicar = () => alert('Clicou')
+
+
+return <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding:
 12, borderRadius: 8}}>
-<label htmlFor="nome" style={{display: 'block', marginBottom:
-4}}>Nome:</label>
+
+<label className="rotulo" htmlFor="nome" style={{display: 'block', marginBottom:
+4}}>
+    {textoDoRotulo}
+</label>
+
 <input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8,
 borderStyle: 'hidden',width: '100%', borderRadius: 8, outline: 'none', boxSizing:
 'border-box'}}/>
-<button style={{marginTop: 12, paddingTop: 8, paddingBottom: 8,
-backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%',
-borderRadius: 8}}>Enviar</button>
+
+<button 
+    onClick={() => aoClicar()}
+    style={estilosBotao}>
+    {obterTextoDoBotao()}
+</button>
+
 </div>
-)
+
 }
 
 export default App
