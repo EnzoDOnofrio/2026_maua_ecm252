b47d5dfbf640145afb205d13fbd285c960f5f4f2
enzo_donofrio <enzo.oliveira2004@outlook.com>
2026-04-18 10:38:17 -0300

    learn(ap00x): ajusta dotenv

--- ap00x_back_front_integracao/backend/server.js
+++ ap00x_back_front_integracao/backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const mysql2 = require('mysql2/promise')
 
@@ -10,11 +11,11 @@ let conexao
 const conectar = async () => {
     try {
         conexao = await mysql2.createConnection({
-            host: 'maua-ecm252-enzoo5820-d4bc.e.aivencloud.com',
-            user: 'avnadmin',
-            password: 'AVNS_0_J6U-Xs0RyvBJ8Tdht',
-            database: 'defaultdb',
-            port: 17101
+            host: process.env.HOST,
+            user: process.env.USUARIO,
+            password: process.env.PASSWORD,
+            database: process.env.DATABASE,
+            port: process.env.PORT
         })
         console.log('Conectado ao MySQL')
     }
